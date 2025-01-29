3f1e9c8969cb77b44f590615d05bf40b33e644d0
Sularada <111848753+Sularada@users.noreply.github.com>
2025-01-29 16:10:51 +0300

    Calculater project V1 finished.

--- sources/main.js
+++ sources/main.js
@@ -23,7 +23,26 @@ const ans = document.getElementById("ans");
 let ans_content = document.getElementById("ans").textContent;
 const history_element = document.getElementById("history");
 let history_items = [];
-let history_list = document.getElementById("historyList");
+const history_list = document.getElementById("historyList");
+
+const ans_button = document
+  .getElementById("ansButton")
+  .addEventListener("click", ansFunction);
+let last_answer = "";
+
+all_buttons = document.getElementsByTagName("button");
+
+Array.prototype.forEach.call(all_buttons, (button) => {
+  button.addEventListener("click", clickEffectFunction);
+});
+
+function ansFunction() {
+  ops_content = last_answer;
+
+  ans_content = "";
+  ans.innerHTML = ans_content;
+  ops.innerHTML = ops_content;
+}
 
 function clearFunction() {
   ans_content = "";
@@ -41,14 +60,10 @@ function deleteFunction() {
 
 function historyFunction() {
   console.log(history_items);
-  if (history_element.style.display == "none") {
-    setTimeout(() => {
-      history_element.style.display = "block";
-    }, 500);
+  if (!history_element.classList.contains("clicked")) {
+    history_element.classList.add("clicked");
   } else {
-    setTimeout(() => {
-      history_element.style.display = "none";
-    }, 500);
+    history_element.classList.remove("clicked");
   }
 }
 
@@ -56,6 +71,14 @@ function historyListUpdateFunction(item) {
   let li = document.createElement("li");
   li.innerText = item;
   history_list.appendChild(li);
+  li.addEventListener("click", selectedFunction);
+}
+
+function selectedFunction() {
+  ans_content = "";
+  ops_content = this.textContent;
+  ans.innerHTML = ans_content;
+  ops.innerHTML = ops_content;
 }
 
 Array.prototype.forEach.call(items, (item) => {
@@ -73,4 +96,14 @@ function equalOpFunction() {
   historyListUpdateFunction(ops_content);
   ans_content = eval(ops_content.replace("÷", "/").replace("×", "*"));
   ans.innerHTML = ans_content;
+  last_answer = ans_content;
+}
+
+function clickEffectFunction() {
+  var button = this;
+  button.classList.add("clicked");
+
+  setTimeout(function () {
+    button.classList.remove("clicked");
+  }, 300);
 }
